18376e916eabf066d24fe8d18ccd266343084fc9
Leonardo Freitas <leonardo.paulino.ps@ojc.com.br>
2025-03-10 00:09:10 -0300

    feat: new impl

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+set -g PATH $HOME/.local/bin $PATH
+
+set -g fish_greeting
+
+zoxide init fish | source
+
+starship init fish | source
+
+# GJC
+set -Ux MAVEN_ALL_REPO_URL 'https://nexus.gjccorp.com.br/repository/maven-all' 
+set -Ux MAVEN_RELEASES_REPO_URL 'https://nexus.gjccorp/repository/maven-releases'
+set -Ux MAVEN_SNAPSHOTS_REPO_URL 'https://nexus.gjccorp.com.br/repository/maven-snapshots'
+
+alias kube-az 'az aks get-credentials --resource-group rg-aks-corp-prd-eus2 --name aks-corp-prd-eus2'
+alias kube-qa 'kubectl config use-context k8s-cluster-qa'
