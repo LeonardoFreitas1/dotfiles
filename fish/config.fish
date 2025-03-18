3954fe6d72df369908007651b803942c872975bc
Leonardo Freitas <leonardo.paulino.ps@ojc.com.br>
2025-03-17 22:06:47 -0300

    feat: add fish config

---
+++ fish/config.fish
@@ -0,0 +1,32 @@
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+set -g PATH $HOME/.local/bin $PATH
+
+set -g fish_greeting
+
+zoxide init fish | source
+
+starship init fish | source
+
+alias kube-az 'az aks get-credentials --resource-group rg-aks-corp-prd-eus2 --name aks-corp-prd-eus2'
+alias kube-qa 'kubectl config use-context k8s-cluster-qa'
+
+fzf --fish | source
+
+set KUBE_EDITOR '/usr/sbin/nvim'
+
+if test -f ./.fish_secrets
+    source ./.fish_secrets
+end
